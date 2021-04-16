08f9520dc2faef42bd40fe62fc37b147f8ef746a
oramirezperera <ramirez.orlando17@gmail.com>
2021-04-15 21:46:46 -0400

    playing around with dictionaries comprehensions

---
+++ hw2.py
@@ -0,0 +1,9 @@
+""" create a dictionary with the first thousand numbers and their square root"""
+from math import sqrt
+
+def run():
+    my_dict = {i:sqrt(i) for i in range(1,1001)}
+    print(my_dict)
+
+if __name__ == '__main__':
+    run()
